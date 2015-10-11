38354ccf8863cd01dc11f51cb25e0e42d4cfc874
private <user@example.com>
2015-10-11 21:21:46 +0100

    Adds the conditions to check for a spare in each frame.

--- test/com/bowlingGame/BowlingGameTest.java
+++ test/com/bowlingGame/BowlingGameTest.java
@@ -43,14 +43,14 @@ public class BowlingGameTest {
         assertEquals(20, game.score());
     }
 
-//    @Test
-//    public void oneSpare() {
-//        game.roll(5);
-//        game.roll(5); // spare
-//        game.roll(3);
-//
-//        rollMany(17, 0);
-//
-//        assertEquals(16, game.score());
-//    }
+    @Test
+    public void oneSpare() {
+        game.roll(5);
+        game.roll(5); // spare
+        game.roll(3);
+
+        rollMany(17, 0);
+
+        assertEquals(16, game.score());
+    }
 }
