e82c67d10ed327aea5208396edc2c52ed81b2c11
private <user@example.com>
2015-10-11 23:41:54 +0100

    Adds the fifth test, for the perfect game.

--- test/com/bowlingGame/BowlingGameTest.java
+++ test/com/bowlingGame/BowlingGameTest.java
@@ -60,6 +60,12 @@ public class BowlingGameTest {
         assertEquals(24, game.score());
     }
 
+    @Test
+    public void perfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, game.score());
+    }
+
     private void rollStrike() {
         game.roll(10);
     }
