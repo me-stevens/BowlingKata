38bc3df4214767395641d1f054ca3c85732d83f1
private <user@example.com>
2015-10-11 20:46:20 +0100

    Places responsabilities in the correct place, now the score function calculates the score, as expected.

--- src/com/bowlingGame/Game.java
+++ src/com/bowlingGame/Game.java
@@ -5,17 +5,18 @@ package com.bowlingGame;
  */
 public class Game {
 
-    private int score       = 0;
     private int[] rolls     = new int[21];
     private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
         rolls[currentRoll] = pins;
         currentRoll++;
     }
 
     public int score() {
+        int score = 0;
+        for(int i=0; i<rolls.length; i++)
+            score += rolls[i];
         return score;
     }
 }
