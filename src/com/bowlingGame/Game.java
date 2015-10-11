ea9d89979cc23224fc4a212af0486a5faf9f604d
private <user@example.com>
2015-10-11 21:36:50 +0100

    Extracts the spare condition into its own method.

--- src/com/bowlingGame/Game.java
+++ src/com/bowlingGame/Game.java
@@ -19,7 +19,7 @@ public class Game {
 
         for(int frame=0; frame<10; frame++) {
 
-            if(rolls[frameIndex] + rolls[frameIndex+1] == 10) { // spare
+            if(isSpare(frameIndex)) {
                 score += 10 + rolls[frameIndex+2];
                 frameIndex += 2;
             }
@@ -31,5 +31,9 @@ public class Game {
         }
 
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex+1] == 10;
     }
 }
