dc199cc156260aa9729c4391bef42c4ddd59474a
private <user@example.com>
2015-10-11 23:10:58 +0100

    Adds the condition for a strike, needed to make the test pass.

--- src/com/bowlingGame/Game.java
+++ src/com/bowlingGame/Game.java
@@ -19,7 +19,12 @@ public class Game {
 
         for(int frame=0; frame<10; frame++) {
 
-            if(isSpare(frameIndex)) {
+            if(rolls[frameIndex] == 10) { // strike
+                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                frameIndex++;
+            }
+
+            else if(isSpare(frameIndex)) {
                 score += 10 + rolls[frameIndex+2];
                 frameIndex += 2;
             }
