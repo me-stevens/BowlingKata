1dea7e19863ed68d7f69bd826c6ce92d90a86c0b
private <user@example.com>
2015-10-11 20:37:40 +0100

    Comments Test Three because a redesign of the initial idea is needed.

--- src/com/bowlingGame/Game.java
+++ src/com/bowlingGame/Game.java
@@ -5,10 +5,14 @@ package com.bowlingGame;
  */
 public class Game {
 
-    private int score = 0;
+    private int score       = 0;
+    private int[] rolls     = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
         score += pins;
+        rolls[currentRoll] = pins;
+        currentRoll++;
     }
 
     public int score() {
